9a61ca2e84c8b6c3b4f1f00825d9bf6ae429d199
Tim Camper <twcamper@thoughtworks.com>
2013-06-17 14:25:35 -0500

    ex 1.1.5 - compound logical expression

---
+++ src/main/java/01/_1_1_5_InRange.java
@@ -0,0 +1,23 @@
+public class _1_1_5_InRange {
+
+  static boolean inRange(double n) {
+    if (n > 0.0 && n < 1.0)
+      return true;
+
+    return false;
+  }
+  public static void main(String[] args)
+  {
+    if (args.length == 2) {
+      double x = Double.parseDouble(args[0]);
+      double y = Double.parseDouble(args[1]);
+
+      if (inRange(x) && inRange(y))
+        StdOut.println("true");
+      else
+        StdOut.println("false");
+    } else {
+      System.err.println("Two doubles are required");
+    }
+  }
+}
