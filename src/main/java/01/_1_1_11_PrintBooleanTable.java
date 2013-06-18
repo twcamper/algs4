b154e46a526a31d2034ff4f041dff8ce9afc5d40
Tim Camper <twcamper@thoughtworks.com>
2013-06-18 10:29:03 -0500

    ex 1.1.11 print a table

---
+++ src/main/java/01/_1_1_11_PrintBooleanTable.java
@@ -0,0 +1,18 @@
+public class _1_1_11_PrintBooleanTable {
+
+  public static void main(String[] args) {
+    // boolean[][] table = new boolean[10][10];
+    boolean[][] table = {
+      {true, true, true, false, true},
+      {true, false, true, false, true},
+      {true, true, true, false, true},
+      {true, false, true, false, true},
+      {true, false, true, false, true},
+      {true, false, true, false, true}
+    };
+    for (int row = 0; row < table.length; row++)
+      for (int col = 0; col < table[row].length; col++) {
+        StdOut.printf("%d, %d: %s\n", row, col, (table[row][col] ? "*" : ""));
+      }
+  }
+}
