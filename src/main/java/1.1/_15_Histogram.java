c36a26d50785dc432d73668742f399a8596b4e2a
Tim Camper <twcamper@thoughtworks.com>
2013-06-18 20:45:11 -0500

    ex 1.1.15 - simplify

--- src/main/java/1.1/_15_Histogram.java
+++ src/main/java/1.1/_15_Histogram.java
@@ -18,22 +18,22 @@ public class _15_Histogram {
     if (args.length < 3)
       System.err.println("At least 3 integers required");
     else {
-      int[] intArgs = new int[args.length];
-      for (int i = 0; i < intArgs.length; i++)
-        intArgs[i] = Integer.parseInt(args[i]);
+      int range = Integer.parseInt(args[0]);
+      int[] values = new int[args.length - 1];
+      for (int i = 1; i < values.length; i++)
+        values[i-1] = Integer.parseInt(args[i]);
 
-      // range 'to' is exclusive, thus we don't use len - 1
-      int h[] = histogram(Arrays.copyOfRange(intArgs, 1, intArgs.length), intArgs[0]);
+      int h[] = histogram(values, range);
       
-      int sumOfH = 0;
+      int sum = 0;
       for (int i = 0; i < h.length; i++) {
         StdOut.printf("%d\t", h[i]);
-        sumOfH += h[i];
+        sum += h[i];
       }
       StdOut.println();
 
       // java -ea
-      assert (sumOfH == (intArgs.length - 1)) : "Histogram fails to sum correctly: expected " + (intArgs.length - 1);
+      assert (sum == values.length) : "Histogram fails to sum correctly: expected " + values.length;
     }
   }
 }
