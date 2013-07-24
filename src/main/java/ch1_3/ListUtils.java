160f0515910bb45a687942286e1eff75e1f60d49
Tim Camper <twcamper@thoughtworks.com>
2013-07-23 22:44:29 -0500

    ex 1.3.24 - removeAfter()

--- src/main/java/ch1_3/ListUtils.java
+++ src/main/java/ch1_3/ListUtils.java
@@ -6,20 +6,44 @@ import util.Node;
 public class ListUtils
 {
 
-  public static <Item> Node<Item> insert(Node<Item> list, Item i)
+  public static <I> void append(Node<I> list, I i)
   {
-    Node<Item> newHead = new Node<Item>();
+    Node<I> n = list;
+    while (n.next != null)
+      n = n.next;
+    n.next = new Node<I>();
+    n.next.item = i;
+  }
+  public static <I> Node<I> insert(Node<I> list, I i)
+  {
+    Node<I> newHead = new Node<I>();
     newHead.item = i;
     newHead.next = list;
     return newHead;
   }
 
-  public static <Item> boolean find(Node<Item> list, Item i)
+  public static <I> boolean find(Node<I> list, I i)
   {
-    for (Node<Item> n = list; n != null; n = n.next) {
+    for (Node<I> n = list; n != null; n = n.next) {
       if (n.item.equals(i))
         return true;
     }
     return false;
+  }
+
+  public static <I> void removeAfter(Node<I> node)
+  {
+    if (node.next == null)
+      return;
+
+    node.next = node.next.next;
+  }
+
+  public static <I> int size(Node<I> list)
+  {
+    int count = 0;
+    for (Node<I> n = list; n != null; count++, n = n.next)
+      ;
+    return count;
   }
 }
--- src/test/java/ch1_3/ListUtilsTest.java
+++ src/test/java/ch1_3/ListUtilsTest.java
@@ -20,27 +20,27 @@ public class ListUtilsTest {
   public void setUp()
   {
     l = new Node<String>();
-    l.item = "foo";
-    l = ListUtils.insert(l, "bar");
-    l = ListUtils.insert(l, "clave");
-    l = ListUtils.insert(l, "samba");
+    l.item = "tail";
+    l = ListUtils.insert(l, "third");
+    l = ListUtils.insert(l, "second");
+    l = ListUtils.insert(l, "head");
   }
   @Test
   public void _21_findFirst()
   {
-    assertEquals(true, ListUtils.find(l, "samba"));
+    assertEquals(true, ListUtils.find(l, "head"));
   }
 
   @Test
   public void _21_findLast()
   {
-    assertEquals(true, ListUtils.find(l, "foo"));
+    assertEquals(true, ListUtils.find(l, "tail"));
   }
 
   @Test
   public void _21_findMiddle()
   {
-    assertEquals(true, ListUtils.find(l, "clave"));
+    assertEquals(true, ListUtils.find(l, "second"));
   }
 
   @Test
@@ -49,4 +49,42 @@ public class ListUtilsTest {
     assertEquals(false, ListUtils.find(l, "Nerd Ball Pudding"));
   }
 
+  @Test
+  public void _24_removeAfterFirst()
+  {
+    ListUtils.removeAfter(l);
+    assertEquals(false, ListUtils.find(l, "second"));
+    assertThat(l.next.item, equalTo("third"));
+  }
+
+  @Test
+  public void _24_removeAfterMiddle()
+  {
+    Node<Integer> head = new Node<Integer>();
+    Node<Integer> middle = new Node<Integer>();
+    Node<Integer> tail = new Node<Integer>();
+
+    head.item = 10;
+    middle.item = 20;
+    tail.item = 30;
+    head.next = middle;
+    middle.next = tail;
+
+    ListUtils.removeAfter(middle);
+    assertThat(middle.next, equalTo(null));
+  }
+
+  @Test
+  public void _24_removeAfterLast()
+  {
+    Node<Integer> head = new Node<Integer>();
+    Node<Integer> tail = new Node<Integer>();
+
+    head.item = 1;
+    head.next = tail;
+    tail.item = 2;
+
+    ListUtils.removeAfter(tail);
+    assertEquals(2, ListUtils.size(head));
+  }
 }
