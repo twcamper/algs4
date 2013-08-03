96d2b96be624cf60dca635bffa15bbdac57c87eb
Tim Camper <twcamper@thoughtworks.com>
2013-08-03 11:50:11 -0500

    ex 1.3.37 - Josephus' problem: every nth in a circle

--- src/main/java/ch1_3/_37_Josephus.java
+++ src/main/java/ch1_3/_37_Josephus.java
@@ -1,52 +1,53 @@
 import util.*;
+/*
+   1.3.37 Josephus problem. In the Josephus problem from antiquity, N people are in dire
+   straits and agree to the following strategy to reduce the population. They arrange themselves
+   in a circle (at positions numbered from 0 to N-1) and proceed around the circle,
+   eliminating every Mth person until only one person is left. Legend has it that Josephus
+   figured out where to sit to avoid being eliminated. Write a Queue client Josephus that
+   takes M and N from the command line and prints out the order in which people are
+   eliminated (and thus would show Josephus where to sit in the circle).
+*/
 
 public class _37_Josephus
 {
-
   private static int nextIndex(int size, int current)
-  {
-    return (current < size - 1) ? current + 1 : 0;
-  }
-  public static Queue<Integer> buildQueue(int interval, int N)
-  {
-    Integer[] people = new Integer[N];
-    for (int i = 0; i < people.length; i++)
-      people[i] = i;
-    Queue<Integer> q = new Queue<>();
-    int i = 0;
-    int intervalCounter = 0;
-    while (q.size() < N) {
-      if (intervalCounter == interval) {
-        // if (people[i] == null)
-          // i = nextIndex(people.length, i);
-        q.enqueue(people[i]);
-        people[i] = null;
-        intervalCounter = 0;
-      } else {
-        intervalCounter++;
-        i = nextIndex(people.length, i);
-      }
+  { return (current < size - 1) ? current + 1 : 0; }
 
+  public static Queue<Integer> buildQueue(int ordinal, int N)
+  {
+    // circle of elements (people?)
+    Integer[] circle = new Integer[N];
+    for (int i = 0; i < circle.length; i++)
+      circle[i] = i;
+
+    // queue that holds the order of elimination:
+    // the last one survives!
+    Queue<Integer> q = new Queue<>();
+    int            i = 0;
+    int      skipped = 0;
+    int       toSkip = ordinal - 1;
+
+    while (q.size() < N) {
+      if (skipped < toSkip)
+        skipped++;
+      else {
+        // when N is an even multiple of ordinal,
+        // we'll hit the position of a previously
+        // eliminated element.
+        if (circle[i] == null)
+          i = nextIndex(circle.length, i);
+
+        q.enqueue(circle[i]);
+
+        // indicate the element has been eliminated
+        circle[i] = null;
+
+        // reset to find next element to eliminate
+        skipped = 0;
+      }
+      i = nextIndex(circle.length, i);
     }
-    /*
-     * init array of size 'people' to ints 0 to N-1
-     * interval counter = 0
-     * array index = 0
-     * while queue size < 'people'
-     *   if interval counter < interval
-     *     increment interval counter
-     *     if array index < array size
-     *       increment array index
-     *     else
-     *       reset array index to 0
-     *   else
-     *     if array[index] == null
-     *       increment array index
-     *     enqueue a copy of the val array[index]
-     *     set array[index] = null
-     *     reset interval counter to 0
-     *
-     */
     return q;
   }
   public static void main(String[] args)
@@ -58,8 +59,6 @@ public class _37_Josephus
     Queue<Integer> q = buildQueue(Integer.parseInt(args[0]), Integer.parseInt(args[1]));
 
     System.out.println(q);
-    for (Integer i : q)
-      System.out.println(i);
 
     /* 2 7
      * 1 3 5 0 2 4 6
@@ -80,7 +79,16 @@ public class _37_Josephus
      * 3 0 4 1 5 2 6
      *
      * 3 6
-     * 2 5 3
+     * 2 5 3 0 4 1
+     *
+     * 3 3
+     * 2 0 1
+     *
+     * 6 6
+     * 5 0 1 2 3 4
+     *
+     * 1 6
+     * 0 1 2 3 4 5
      */
 
   }
